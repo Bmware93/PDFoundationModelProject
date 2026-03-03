35326029450a63f7727bf77da484568e7c294952
Benia Morgan <bmorganware@gmail.com>
2026-03-03 15:22:14 -0500

    created view for ingredient view

---
+++ FoundationModelDemo/IngredientHeader.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientHeader.swift
+//  FoundationModelDemo
+//
+//  Created by Benia Morgan-Ware on 3/3/26.
+//
+
+import SwiftUI
+
+struct IngredientHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientHeader()
+}
